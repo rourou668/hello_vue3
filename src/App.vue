d77b3aeda82bdc504387057c915877e1f4139d26
rourou668 <2145775895.@qq.com>
2026-03-12 15:52:25 +0800

    vue3 01_自己写一个APP组件

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+//JS或TS
+// 交互
+export default {
+  name: 'App', //组件名
+}
+</script>
+
+<template>
+  <!-- hmtl -->
+  <!-- 结构 -->
+  <div class="app">
+    <h1>我好啊！</h1>
+  </div>
+</template>
+
+<style>
+/* 样式 */
+.app {
+  background-color: aliceblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
