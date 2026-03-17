81581fb467cbb45f0eabe2531f439cc99d85cf57
rourou668 <2145775895.@qq.com>
2026-03-18 00:24:56 +0800

    10_toRefs与toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,50 +1,44 @@
-<!-- 若需要一个基本类型的响应式数据，必须使用ref -->
-<!-- 若需要一个响应式对象，层级不深，ref、reactive都可以 -->
-<!-- 若需要一个响应式对象，且层级较深，推荐使用reactive -->
-
 <script lang="ts" setup>
+import { reactive, toRefs, toRef } from 'vue'
+
 defineOptions({
-  name: 'Person123', //组件名
+  name: 'Person123',
 })
 
-import { reactive, ref } from 'vue'
 //数据
-let car = reactive({ brand: '奔驰', price: 100 }) //reactive重新分配一个新对象，会失去响应式（可以使用Object.assign去整体替换）
-let sum = ref(0)
+let person = reactive({
+  name: '张三',
+  age: 18,
+})
+
+//解构赋值
+// let { name, age } = person
+//相当于把person.name赋值给name,创建了一个新变量。此时，person.name是响应式数据
+
+let { name, age } = toRefs(person)
+//此时，name、age为响应式数据
+console.log(name)
+console.log(age)
+
+let nl = toRef(person, 'age')
+console.log(nl.value)
 
 //方法
-function changePrice() {
-  car.price += 10
+function changeName() {
+  name.value += '~'
+  // console.log(name, person.name)
 }
-
-function changeBrand() {
-  car.brand = '宝马'
-}
-
-function changeCar() {
-  //这么写页面不更新
-  // car = { brand: '法拉利', price: 1000 } ,但是ref可以直接这么写，记得加.value
-  // car = reactive({ brand: '法拉利', price: 1000 })
-
-  //页面可以更新
-  Object.assign(car, { brand: '法拉利', price: 1000 })
-  //Object.assign(a,b,c)
-  //把b、c中的属性都加到a中
-}
-
-function changeSum() {
-  sum.value += 1
+function changeAge() {
+  age.value += 1
 }
 </script>
 
 <template>
   <div class="person">
-    <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-    <button @click="changePrice">修改汽车的价格</button>
-    <button @click="changeBrand">修改汽车的品牌</button>
-    <button @click="changeCar">修改汽车</button>
-    <h2>当前求和为{{ sum }}</h2>
-    <button @click="changeSum">点我sum+1</button>
+    <h2>{{ person.name }}</h2>
+    <h2>{{ person.age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
   </div>
 </template>
 
