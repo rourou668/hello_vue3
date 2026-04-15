6c66ecf039f790034d24a6135598720f436e482b
rourou668 <2145775895.@qq.com>
2026-04-16 00:18:26 +0800

    13_watch监视_情况一

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -11,30 +11,45 @@ defineOptions({
 
 import { ref, watch } from 'vue'
 
-//数据
-let sum = ref(0)
+// 数据
+let person = ref({
+  name: '张三',
+  age: 18,
+})
 
 // 方法
-function changeSum() {
-  sum.value += 1
+function changeName() {
+  person.value.name += '~'
+}
+function changeAge() {
+  person.value.age += 1
+}
+function changePerson() {
+  person.value = { name: '李四', age: 90 }
 }
 
 // 监视
-// watch(谁？，回调函数)
-// 注意sum后面不要加.value
-const stopWatch = watch(sum, (newValue, oldValue) => {
-  console.log('sum变化了', newValue, oldValue)
-  if (newValue >= 10) {
-    stopWatch()
-  }
-})
+// 情况一：监视【ref】定义的【对象类型】数据，监视的是对象的地址值，若想监视对象内部属性的变化，需要手动开启深度监视
+//watch的第一个参数：被监视的数据
+// watch的第二个参数：监视的回调
+// watchc的第三个参数：配置对象（deep、immediate等等）
+watch(
+  person,
+  (newValue, oldValue) => {
+    console.log('person变化了', newValue, oldValue)
+  },
+  { deep: true, immediate: true },
+)
 </script>
 
 <template>
   <div class="person">
-    <h1>情况一：监视【ref】定义的基本型数据</h1>
-    <h2>当前求和为：{{ sum }}</h2>
-    <button @click="changeSum">点我sum+1</button>
+    <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="changePerson">all in</button>
   </div>
 </template>
 
